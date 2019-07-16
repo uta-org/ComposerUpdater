9bc69fd68e432007f13ce26d950c6bb99d2c68be
z3nth10n <z3nth10n@gmail.com>
2019-07-16 22:32:55 +0200

    Initial commit

---
+++ ComposerUpdater/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace ComposerUpdater
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string webdir = Path.Combine(Path.GetDirectoryName(Environment.CurrentDirectory), "web");
+
+            var composerFiles = Directory.GetFiles(webdir, "*.json", SearchOption.AllDirectories).Where(file =>
+                Path.GetFileNameWithoutExtension(file)?.ToLowerInvariant() == "composer");
+
+            foreach (var file in composerFiles)
+            {
+                // Process.Start("composer", "install")
+                using (var process =
+                    new Process
+                    {
+                        StartInfo =
+                        new ProcessStartInfo("composer", "install")
+                        {
+                            WorkingDirectory = Path.GetDirectoryName(file),
+                            UseShellExecute = false,
+                            CreateNoWindow = true,
+                            RedirectStandardOutput = true
+                        }
+                    })
+                {
+                    string output = process.StandardOutput.ReadToEnd();
+                    Console.WriteLine(output);
+
+                    process.WaitForExit();
+                }
+            }
+
+            Console.WriteLine("All composer packages installed succesfully!");
+            Console.ReadLine();
+        }
+    }
+}
