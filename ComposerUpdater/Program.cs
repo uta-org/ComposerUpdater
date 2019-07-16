495063c26e1195a99578e58756cf6392158a8e3f
z3nth10n <z3nth10n@gmail.com>
2019-07-16 22:36:00 +0200

    Added colorful console

--- ComposerUpdater/Program.cs
+++ ComposerUpdater/Program.cs
@@ -3,41 +3,57 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 
+using System.Drawing;
+using Console = Colorful.Console;
+
 namespace ComposerUpdater
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
-            string webdir = Path.Combine(Path.GetDirectoryName(Environment.CurrentDirectory), "web");
-
-            var composerFiles = Directory.GetFiles(webdir, "*.json", SearchOption.AllDirectories).Where(file =>
-                Path.GetFileNameWithoutExtension(file)?.ToLowerInvariant() == "composer");
-
-            foreach (var file in composerFiles)
+            try
             {
-                // Process.Start("composer", "install")
-                using (var process =
-                    new Process
-                    {
-                        StartInfo =
-                        new ProcessStartInfo("composer", "install")
-                        {
-                            WorkingDirectory = Path.GetDirectoryName(file),
-                            UseShellExecute = false,
-                            CreateNoWindow = true,
-                            RedirectStandardOutput = true
-                        }
-                    })
-                {
-                    string output = process.StandardOutput.ReadToEnd();
-                    Console.WriteLine(output);
+                string webdir = Path.Combine(Path.GetDirectoryName(Environment.CurrentDirectory), "web");
 
-                    process.WaitForExit();
+                var composerFiles = Directory.GetFiles(webdir, "*.json", SearchOption.AllDirectories).Where(file =>
+                    Path.GetFileNameWithoutExtension(file)?.ToLowerInvariant() == "composer");
+
+                foreach (var file in composerFiles)
+                {
+                    // Process.Start("composer", "install")
+                    using (var process =
+                        new Process
+                        {
+                            StartInfo =
+                            new ProcessStartInfo("composer", "install")
+                            {
+                                WorkingDirectory = Path.GetDirectoryName(file),
+                                UseShellExecute = false,
+                                CreateNoWindow = true,
+                                RedirectStandardOutput = true
+                            }
+                        })
+                    {
+                        string output = process.StandardOutput.ReadToEnd();
+                        Console.WriteLine(output);
+
+                        process.WaitForExit();
+                    }
                 }
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex, Color.Red);
 
-            Console.WriteLine("All composer packages installed succesfully!");
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine("All composer packages installed succesfully!", Color.Lime);
+
+            Console.WriteLine("Press any key to exit...");
             Console.ReadLine();
         }
     }
